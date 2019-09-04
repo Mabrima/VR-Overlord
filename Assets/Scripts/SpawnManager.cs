bd565941c223d927c5bb36c3649e0afd51a74f74
Klutchie <43027043+Klutchie@users.noreply.github.com>
2019-09-04 11:30:18 +0200

    Added spawnsystem and Gamemanager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public int enemiesPerWave;
+    public GameObject enemyPrefab;
+}
+
+
+public class SpawnManager : MonoBehaviour
+{
+    #region Public variables
+    public Wave[] waves;
+    public Transform spawnPoint;
+    public float timeBetweenEnemies;
+    #endregion
+
+    #region Private variables
+    private int totalEnemiesInCurrentWave;
+    private int enemiesInWaveLeft;
+    private int spawnedEnemies;
+    [SerializeField]
+    private int currentWave;
+    private int totalWaves;
+    private int currentWavesDefeated;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWave -= 1;
+        //-1 on length because we are starting on index 0.
+        totalWaves = waves.Length - 1;
+        StartNextWave();
+    }
+
+    void StartNextWave()
+    {
+        currentWave++;
+
+        //Win
+        if (currentWave > totalWaves)
+        {
+            currentWavesDefeated++;
+
+            if (currentWavesDefeated % 3 == 0) //Every third wave defeated go to shop/whatever.
+            {
+                if (currentWavesDefeated != 0) //dont remember if this is neccessary..
+                {
+                    //Do something after every third wave.
+                }
+
+            }
+            return;
+        }
+        totalEnemiesInCurrentWave = waves[currentWave].enemiesPerWave;
+        enemiesInWaveLeft = 0;
+        spawnedEnemies = 0;
+
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        //Spawn enemies while amount of enemies are under predetermined enemy amount.
+        while (spawnedEnemies < totalEnemiesInCurrentWave)
+        {
+            
+            spawnedEnemies++;
+            enemiesInWaveLeft++;
+
+            //Instantiate enemy
+            GameObject enemy = waves[currentWave].enemyPrefab;
+            Instantiate(enemy, spawnPoint.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+        yield return null;
+
+    }
+
+    public void EnemiesDefeated() //If we have multiple waves.  --  Call this whenever and enemy dies.
+    {
+        enemiesInWaveLeft--;
+
+        if (enemiesInWaveLeft == 0 && spawnedEnemies == totalEnemiesInCurrentWave)
+        {
+            
+            StartNextWave();
+        }
+
+    }
+
+    //Start waves again.
+    public void NextLevel() //If we only have 1 wave.
+    {
+        currentWave = -1;
+        StartNextWave();
+    }
+}
