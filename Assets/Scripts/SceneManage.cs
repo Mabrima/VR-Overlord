1cb6b8ed2a18b6f891599797975d80c9437f7b98
Philip <philipa@hotmail.se>
2019-09-05 15:00:19 +0200

    Explotion

--- Assets/Scripts/SceneManage.cs
+++ Assets/Scripts/SceneManage.cs
@@ -9,8 +9,7 @@ public class SceneManage : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        MenuScene();
+    { 
     }
 
 
@@ -21,7 +20,7 @@ public class SceneManage : MonoBehaviour
 
     public void Game()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("World Robin");
     }
 
     public void Quit()
