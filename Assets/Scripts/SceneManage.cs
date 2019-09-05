575abb0ec3665da0d17a8b50198c392fdb6a74bc
Philip <philipa@hotmail.se>
2019-09-05 13:20:25 +0200

    Update menu

---
+++ Assets/Scripts/SceneManage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuScene();
+    }
+
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
