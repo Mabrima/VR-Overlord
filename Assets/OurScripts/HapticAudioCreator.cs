bb990cc9b723707628e6de34842a7a5fb6f60a19
Mabrima <robinkarlola.arkblad@gmail.com>
2019-09-06 11:41:35 +0200

    more creatures more animations

---
+++ Assets/OurScripts/HapticAudioCreator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticAudioCreator : MonoBehaviour
+{
+    public static HapticAudioCreator singleton;
+
+    private void Awake()
+    {
+        if (singleton)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            singleton = this;
+        }
+    }
+
+    /// <summary>
+    /// Takes in 3 ints
+    /// Iteration; how long the clip will be, 360 is a full second as the controllers run at 360HZ.
+    /// Frequency; how often of these 360HZ it will rumble, ex 2 will run it every other, 4 every 4th and so on.
+    /// Strength; how strong the vibration will be when it triggers 0-255.
+    /// </summary>
+    /// <param name="iteration"></param>
+    /// <param name="frequency"></param>
+    /// <param name="strength"></param>
+    /// <returns></returns>
+    public OVRHapticsClip CreateHapticAudio(int iteration, int frequency, int strength)
+    {
+        OVRHapticsClip clip = new OVRHapticsClip();
+
+        for (int i = 0; i < iteration; i++)
+        {
+            clip.WriteSample(i % frequency == 0 ? (byte)strength : (byte) 0);
+        }
+
+
+        return clip;
+    }
+}
--- Assets/OurScripts/RockSpawner.cs
+++ Assets/OurScripts/RockSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OVR.OpenVR;
 
 public class RockSpawner : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class RockSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
