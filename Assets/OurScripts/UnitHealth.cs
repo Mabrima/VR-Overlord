9c7b4e711f6cd48f53e0be1f0d2ef6417c81279d
Johan Appelgren <johapp2001@skola.goteborg.se>
2019-09-09 12:16:38 +0200

    Added UnitHealth script for all units that will have health in any form.

---
+++ Assets/OurScripts/UnitHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviour
+{
+    [SerializeField] int health = 1;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        health = health <= 0 ? 0 : health - damage;
+    }
+}
