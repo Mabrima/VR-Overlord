26bd3e9949d1c4a19587320872f9379b4019e974
amiefaily <amie.faily@hotmail.com>
2019-09-09 13:01:14 +0200

    villagehealth

---
+++ Assets/amie/VillageHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageHealth : MonoBehaviour
+{
+    int villageHealth = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        villageHealth = villageHealth - 1;
+
+        if (villageHealth <= 0)
+        {
+            Debug.Log("Village is dead");
+        }
+    }
+
+
+
+
+}
---
+++ Assets/amie/tryhealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class tryhealth : MonoBehaviour
+{
+ public float health;
+ public float maxHealth;
+
+ public GameObject healthBarUI;
+ public Slider slider;
+    private object hit;
+
+    private void Start()
+    {
+        health = maxHealth;
+        slider.value = CalculateHealth();
+    }
+
+    private void Update()
+    {
+        slider.value = CalculateHealth();
+        if (health < maxHealth )
+        {
+            healthBarUI.SetActive(true);
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    float CalculateHealth()
+    {
+        return health / maxHealth;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+           
+            
+        }
+    }
+}
