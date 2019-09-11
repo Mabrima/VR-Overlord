fb8f832009a47af81b3e501b475efcff29bf82c3
Johan Appelgren <johapp2001@skola.goteborg.se>
2019-09-11 10:52:33 +0200

    Removed trigger volume from footman prefab, Changed location of Village and added health and damage functionality to Village.

---
+++ Assets/OurScripts/Village.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Village : MonoBehaviour
+{
+    UnitHealth health;
+
+    private void Start()
+    {
+        health = GetComponent<UnitHealth>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            health.TakeDamage(1);
+            other.GetComponent<UnitHealth>()?.TakeDamage(50);
+        }
+    }
+}
