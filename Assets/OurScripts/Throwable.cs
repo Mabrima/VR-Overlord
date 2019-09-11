9633d7b81c26ee0e84075b9a89b3be84617a36a2
Mabrima <robinkarlola.arkblad@gmail.com>
2019-09-11 13:55:53 +0200

    thrown rocks disapear 10 seconds after landing

--- Assets/OurScripts/Throwable.cs
+++ Assets/OurScripts/Throwable.cs
@@ -13,6 +13,8 @@ public class Throwable : MonoBehaviour
     [SerializeField]
     int damage = 1;
 
+    bool removeRoutineRunning = false;
+
     private void OnTriggerEnter(Collider other)
     {
         other.GetComponent<UnitHealth>()?.TakeDamage(damage);
@@ -23,6 +25,21 @@ public class Throwable : MonoBehaviour
             if (health <= 0)
                 Destroy(gameObject);
         }
+        else if (other.CompareTag("Terrain"))
+        {
+            if (!removeRoutineRunning)
+            {
+                removeRoutineRunning = true;
+                StartCoroutine(RemoveAfterTime());
+            }
+        }
 
+    }
+
+    IEnumerator RemoveAfterTime()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(gameObject);
+        yield return null;
     }
 }
