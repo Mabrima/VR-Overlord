149c692208fbb2c5b0d4507b6e28e719b611936a
Johan Appelgren <johapp2001@skola.goteborg.se>
2019-09-11 15:25:35 +0200

    Changed some settings for the oculus and tested out a reset button for the scene. It does not work as of right now.

---
+++ Assets/OurScripts/ResetScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "LeftControllerAnchor" || other.name == "RightControllerAnchor" || other.name == "Rock")
+        {
+            //This does not work. Crashes to Oculus Menu.
+            SceneManager.LoadScene("World");
+        }
+    }
+}
