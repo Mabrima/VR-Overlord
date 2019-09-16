fad5aff0a61196e8e09c0821586ce666a43b5594
Johan Appelgren <johapp2001@skola.goteborg.se>
2019-09-16 13:12:22 +0200

    Added light bolt prefab to project. Also tweaked and added functionality to it. Also tweaked spider prefab collider.

---
+++ Assets/OurScripts/LightningBolt.cs
@@ -0,0 +1,44 @@
+﻿using DigitalRuby.LightningBolt;
+using System.Collections;
+using UnityEngine;
+
+/* Script Author: Johan Appelgren
+ * Edits by:
+ */
+
+public class LightningBolt : MonoBehaviour
+{
+    [HideInInspector] public GameObject end;
+    [HideInInspector] public GameObject prefab;
+    [SerializeField] int damage = 10;
+
+    void Start()
+    {
+        StartCoroutine(UpdateLightningPosition());
+    }
+
+    IEnumerator UpdateLightningPosition()
+    {
+        do
+        {
+            //Sets lightning end position to the ground
+            if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit))
+                end.transform.position = hit.point;
+            yield return new WaitForSeconds(0.05f);
+        } while (true);
+    }
+
+    public IEnumerator LightningStrike()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit))
+        {
+            prefab.GetComponent<LightningBoltScript>().ChaosFactor = 0.3f;
+            hit.transform.GetComponent<UnitHealth>()?.TakeDamage(damage);
+
+            yield return new WaitForSeconds(0.5f);
+
+            prefab.GetComponent<LightningBoltScript>().ChaosFactor = 0.05f;
+            gameObject.SetActive(false);
+        }
+    }
+}
