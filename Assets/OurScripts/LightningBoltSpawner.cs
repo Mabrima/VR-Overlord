ab32fae17509569af8ab4322407f3db684017dc2
Johan Appelgren <johapp2001@skola.goteborg.se>
2019-09-17 14:27:01 +0200

    Reworked lightning bolt to be a ball you throw that creates a lightning strike at the location it impacts.

---
+++ Assets/OurScripts/LightningBoltSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningBoltSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject lightningBoltPrefab;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Controller") && lightningBoltPrefab.activeInHierarchy == false)
+        {
+            lightningBoltPrefab.transform.position = transform.position;
+            lightningBoltPrefab.SetActive(true);
+        }
+    }
+}
