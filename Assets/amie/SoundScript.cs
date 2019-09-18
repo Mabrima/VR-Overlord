ddc279b30db16f790070da75f94b83ec86b7b231
amiefaily <amie.faily@hotmail.com>
2019-09-18 19:05:03 +0200

    ..

---
+++ Assets/amie/SoundScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+///Amie Faily
+
+public class SoundScript : MonoBehaviour
+{
+    AudioSource ballAudio;
+    string soundName;
+
+    void Start()
+    {
+        ballAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        soundName = collision.gameObject.name;
+        wichSound(soundName);
+    }
+
+    public void wichSound(string soundName)
+    {
+        switch (soundName)
+        {
+            case "Ball Touchdown adio":
+                if (!ballAudio.isPlaying)
+                    ballAudio.Play();
+                break;
+
+            default:
+                if (!ballAudio.isPlaying)
+                    ballAudio.Play();
+                break;
+        }
+    }
+}
