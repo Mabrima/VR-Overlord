70ac06e1112bdc129723922b92db6a4e590f5924
Mabrima <robinkarlola.arkblad@gmail.com>
2019-09-20 15:02:46 +0200

    Update Bouncer.cs

--- Assets/Bouncer.cs
+++ Assets/Bouncer.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class Bouncer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("FireBall"))
